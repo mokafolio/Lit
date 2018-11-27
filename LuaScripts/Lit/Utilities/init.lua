ed7236d886dcad852c64a5e56ebb8a565f7b3215
Matthias Dörfelt <hi@mokafolio.de>
2018-11-26 17:29:53 -0800

    meson

---
+++ LuaScripts/Lit/Utilities/init.lua
@@ -0,0 +1,9 @@
+return
+{
+    math = require("Lit.Utilities.MathUtil"),
+    conv = require("Lit.Utilities.ConversionUtil"),
+    misc = require("Lit.Utilities.MiscUtil"),
+    path = require("Lit.Utilities.PathUtil"),
+    tbl = require("Lit.Utilities.TableUtil"),
+    oop = require("Lit.Utilities.OOPUtil")
+}
--- LuaScripts/Utilities/init.lua
+++
@@ -1,9 +0,0 @@
-return
-{
-    math = require("Utilities.MathUtil"),
-    conv = require("Utilities.ConversionUtil"),
-    misc = require("Utilities.MiscUtil"),
-    path = require("Utilities.PathUtil"),
-    tbl = require("Utilities.TableUtil"),
-    oop = require("Utilities.OOPUtil")
-}
